6d8dfe325fe33d7c7b841ffc76038a19353486ba
Zak Kastl <zkastl8@gmail.com>
2022-12-07 17:07:58 -0600

    finish day5

--- src/day5.rs
+++ src/day5.rs
@@ -3,15 +3,63 @@ use std::fs;
 pub fn puzzle(filepath: &str) {
     println!("\n### ADVENT OF CODE 2022: DAY 5 SOLUTIONS ###");
 
-    let mut counter = 0;
+    let max_stacks = 9;
+    let mut _counter = 0;
+    let mut stacks: Vec<Vec<char>> = Vec::new();
+    let mut data: Vec<&str> = Vec::new();
+    for _i in 0..max_stacks {
+        stacks.push(Vec::<char>::new());
+    }
     for line in fs::read_to_string(filepath).expect("Help! Bad input data!").split("\n") {
+        if line.is_empty() {
+            // format data
+            data.pop();
+            while !data.is_empty() {                
+                let total_char = 2 * max_stacks - 1;
+                let filtered_line: Vec<char> = data.pop().expect("INVALID STRING").chars().filter(|x| x != &'[' && x != &']').collect();
+                assert!(filtered_line.len() == total_char, "{}", line);
+
+                for i in 0..filtered_line.len() {
+                    if i % 2 == 0 && filtered_line[i] != ' ' {
+                        let pos = i/2;
+                        stacks[pos].push(filtered_line[i]);
+                    }
+                }
+            }
+        }
+
+        else {
+            // queue data
+            if !line.contains("move") {
+                data.push(line);
+                continue;
+            }
+
+            let command: Vec<&str> = line.split(" ").collect();
+            assert!(command.len() == 6, "INVALID COMMAND LENGTH");
+            println!("Current line: {}", line);
+            let x = command[1].parse::<usize>().unwrap();
+            let y = command[3].parse::<usize>().unwrap();
+            let z = command[5].parse::<usize>().unwrap();
+            let mut val1: Vec::<char> = Vec::new();
+
+            for _i in 0..x {
+                val1.push(stacks[y-1].pop().unwrap());
+            }
+
+            val1.reverse(); // comment out for part 1
+            stacks[z-1].append(&mut val1);
+
+        }
 
         // Basic plan:
             // Read the source data into two sections. Part 1 / Part 2 / 3
+                    
         // Part 1:
             // Read until the line ' 1  2  3 ...' is read. This marks the final line of section 1,
             // format this data into a vector of vectors (a vector of stacks)
             // This will allow each stack to have the top crate as the last element for 'pop' purposes
+                    
         
         // part 2:
             // Process each line as an action of the crane, e.g 'move 1 from 1 to 2' would mean:
@@ -20,5 +68,15 @@ pub fn puzzle(filepath: &str) {
         // part 3:
             // when part 2 is finished, read the top element of each stack 'blanks do nothing' and concat, return
 
+    // part 3    
+
+    }
+
+    while stacks.len() > 0 {
+        let s = stacks.pop();
+        for c in s.expect("") {
+            print!("{}",c);
+        }
+        println!("");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 fn main() {
     day1::puzzle("C:\\Users\\zkast\\source\\repos\\advent_of_code_22\\day1.txt").expect("help!");
     day2::puzzle("C:\\Users\\zkast\\source\\repos\\advent_of_code_22\\day2.txt").expect("help!");
     day3::puzzle("C:\\Users\\zkast\\source\\repos\\advent_of_code_22\\day3.txt").expect("help!");
     day4::puzzle("C:\\Users\\zkast\\source\\repos\\advent_of_code_22\\day4.txt");
+    day5::puzzle("C:\\Users\\zkast\\source\\repos\\advent_of_code_22\\day5.txt");
     
 }

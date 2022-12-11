be52aac46760eba1d0abc346a82c3d611064ee4b
Zak Kastl <zkastl8@gmail.com>
2022-12-11 17:22:21 -0600

    updates to day7

--- src/day7.rs
+++ src/day7.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, ptr::null};
 
 pub fn puzzle(filepath: &str) -> Result<String, Box<dyn std::error::Error>> {
     println!("\n### ADVENT OF CODE 2022: DAY 7 SOLUTIONS ###");
@@ -6,64 +6,69 @@ pub fn puzzle(filepath: &str) -> Result<String, Box<dyn std::error::Error>> {
     let data = fs::read_to_string(filepath)?;
     let split: Vec::<&str> = data.split("\n").collect();
 
-    for line in 0..split.len() {
-        let cur_line = split[line];
-        let tokens: Vec::<&str> = cur_line.split(" ").collect();
-        assert!(tokens.len() > 0);
-        match tokens[0] {
-            "$" => println!("Command found: {}", tokens[1]),
-
-            _ => println!("Data found: {}", cur_line),
-        }
-    }
+    let file_system = map_filetree(String::from("/"), split, Directory::default());
 
     Ok(data)
 }
 
 fn map_filetree(name: String, history: Vec::<&str>, mut directory: Directory) -> Directory {
 
-    let mut local_map: Directory = Directory { name: name, files: Vec::new(), subdirectories: Vec::new() };
+    // Create the local map
+    let mut local_map: Directory = Directory { 
+        name: name,
+        files: Vec::new(),
+        subdirectories: Vec::new()
+    };
     
     // Reach each line
     for line in 0..history.len() {
         let tokens: Vec::<&str> = history[line].split(" ").collect();
         match tokens[0] {
             "$" => {
+                println!("Processing command: {} {}", tokens[0], tokens[1]);
                 match tokens[1] {
                     "cd" => {
                         match tokens[2] {
                             ".." => {
                                 return local_map;
                             }
-                            &_ => {
-                                map_filetree(tokens[2].to_string(), history, directory);
+
+                            "/" => {
+                                continue;
+                            }
+
+                            &_ => {                               
+                                local_map.subdirectories.push(
+                                    map_filetree(tokens[2].to_string(),
+                                    history[line..history.len()].to_vec(),
+                                    Directory::default()));
+
                             }
                         }
-
+                    }
+                    "ls" => {
+                        continue;
                     }
                     _ => println!("UNSUPPORTED COMMAND")
                 }
             }
 
             "dir" => {
-                let new_dir = Directory {
-                    name: String::from(tokens[1]),
-                    files: Vec::<File>::new(),
-                    subdirectories: Vec::<Directory>::new(),
-                };
-                directory.subdirectories.push(new_dir);
+                println!("Skipping directory line...");
+                continue;
             }
 
-             _ => { }
+            _ => {
+                println!("Found local file: {}", tokens[1]);
+                local_map.files.push(
+                    File {name: tokens[1].to_string(), size: tokens[0].parse::<usize>().unwrap() });
+                    continue;
+
+            }
         }
     }
-    // is line a command?
-    // yes: start new loop, read lines until you reach a new command
-        // follow key words: 'cd' 'ls' 
-    // no: allocate files into the tree
 
-    //return the director
-    Directory::default()
+    return local_map;
 }
 
 struct Directory {
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod day7;
 
 fn main() {
-    day7::puzzle("C:\\Users\\zkast\\source\\repos\\advent_of_code_22\\day7.txt");
+    day7::puzzle("C:\\Users\\zkast\\source\\repos\\advent_of_code_22\\day7.txt").expect("HELP");
     
 }
